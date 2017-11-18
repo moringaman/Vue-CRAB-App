ae3da467d78de648323a12651f6f7525992cc888
Rob Clarke <leon@moringamutual.org>
2017-11-18 14:04:22 +0000

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+
+state: {
+
+},
+
+mutations: {
+
+},
+
+Actions : {
+
+},
+
+getters: {
+  
+}
+
+})
